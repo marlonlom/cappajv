66018bdd73d6272c814a835b3e7b292cf9e8f347
Marlon López <malm.gm@gmail.com>
2024-02-12 17:40:42 -0500

    feat(mobile-app): Updated unit tests for settings viewmodel

--- apps/mobile-app/src/test/kotlin/dev/marlonlom/apps/cappajv/features/settings/SettingsViewModelTest.kt
+++ apps/mobile-app/src/test/kotlin/dev/marlonlom/apps/cappajv/features/settings/SettingsViewModelTest.kt
@@ -10,14 +10,17 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.emptyPreferences
 import dev.marlonlom.apps.cappajv.core.preferences.UserPreferencesRepository
 import dev.marlonlom.apps.cappajv.util.MainDispatcherRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 internal class SettingsViewModelTest {
 
   @get:Rule
@@ -25,26 +28,34 @@ internal class SettingsViewModelTest {
 
   private lateinit var viewModel: SettingsViewModel
 
-  @Before
-  fun setUp() {
-    viewModel = SettingsViewModel(
-      UserPreferencesRepository(
-        FakeSettingsDatastore(flowOf(emptyPreferences()))
-      )
-    )
-  }
-
   @Test
   fun `Should return valid settings from local storage`() = runTest {
-    val setting = viewModel.uiState.value
-    Assert.assertNotNull(setting)
-  }
+    async {
+      viewModel = SettingsViewModel(
+        UserPreferencesRepository(
+          FakeSettingsDatastore(flowOf(emptyPreferences()))
+        )
+      )
 
+      try {
+        val state = viewModel.uiState.first()
+        Assert.assertNotNull(state)
+      } catch (e: Exception) {
+        println(e.message)
+        Assert.fail()
+      }
+    }.await()
+  }
 }
 
-internal class FakeSettingsDatastore(override val data: Flow<Preferences>) : DataStore<Preferences> {
-  override suspend fun updateData(transform: suspend (t: Preferences) -> Preferences): Preferences {
-    return emptyPreferences()
+
+internal class FakeSettingsDatastore(
+  override val data: Flow<Preferences>
+) : DataStore<Preferences> {
+  override suspend fun updateData(
+    transform: suspend (t: Preferences) -> Preferences
+  ): Preferences {
+    return data.first()
   }
 
 }
