611e01e5f6b8b46de62fb80f75b613ebc3a8f15c
Marlon López <malm.gm@gmail.com>
2024-02-12 00:25:06 -0500

    feat(mobile-app): Updated example android ui test

--- apps/mobile-app/src/androidTest/kotlin/dev/marlonlom/apps/cappajv/ExampleInstrumentedTest.kt
+++ apps/mobile-app/src/androidTest/kotlin/dev/marlonlom/apps/cappajv/ExampleInstrumentedTest.kt
@@ -22,6 +22,6 @@ class ExampleInstrumentedTest {
   fun useAppContext() {
     // Context of the app under test.
     val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-    assertEquals("dev.marlonlom.demos.ajv_cappa", appContext.packageName)
+    assertEquals("dev.marlonlom.apps.cappajv", appContext.packageName)
   }
 }
