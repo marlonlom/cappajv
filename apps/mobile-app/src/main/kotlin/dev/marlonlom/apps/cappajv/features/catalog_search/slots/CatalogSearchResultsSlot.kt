209f5dfa557cfe023cb0ab166ceaaef01db988c0
Marlon López <malm.gm@gmail.com>
2024-03-13 21:25:17 -0500

    feat(mobile-app): Updated catalog search slot composables by search ui state

---
+++ apps/mobile-app/src/main/kotlin/dev/marlonlom/apps/cappajv/features/catalog_search/slots/CatalogSearchResultsSlot.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2024 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package dev.marlonlom.apps.cappajv.features.catalog_search.slots
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.runtime.Composable
+import dev.marlonlom.apps.cappajv.features.catalog_search.CatalogSearchUiState
+import dev.marlonlom.apps.cappajv.ui.main.CappajvAppState
+import timber.log.Timber
+
+/**
+ * Catalog search results slot composable ui.
+ *
+ * @author marlonlom
+ *
+ * @param appState Application ui state.
+ * @param searchResultUiState Catalog search result ui state.
+ */
+@ExperimentalFoundationApi
+@Composable
+fun CatalogSearchResultsSlot(
+  appState: CappajvAppState,
+  searchResultUiState: CatalogSearchUiState,
+  onSearchedItemClicked: (Long) -> Unit,
+) {
+  when (searchResultUiState) {
+    CatalogSearchUiState.None -> CatalogSearchWelcomeSlot()
+
+    CatalogSearchUiState.Searching -> CatalogSearchingSlot()
+
+    CatalogSearchUiState.Empty -> CatalogEmptyResultsSlot()
+
+    is CatalogSearchUiState.Success -> CatalogSuccessResultsSlot(
+      searchResults = searchResultUiState.results,
+      onSearchedItemClicked = onSearchedItemClicked
+    )
+  }
+}
