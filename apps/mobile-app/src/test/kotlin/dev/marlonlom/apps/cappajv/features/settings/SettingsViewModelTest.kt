c8c1720c66d99581621e39c1507a9d03207f607d
Marlon López <malm.gm@gmail.com>
2024-03-17 18:02:40 -0500

    feat(mobile-app): Updated unit test for settings viewmodel

--- apps/mobile-app/src/test/kotlin/dev/marlonlom/apps/cappajv/features/settings/SettingsViewModelTest.kt
+++ apps/mobile-app/src/test/kotlin/dev/marlonlom/apps/cappajv/features/settings/SettingsViewModelTest.kt
@@ -5,18 +5,27 @@
 
 package dev.marlonlom.apps.cappajv.features.settings
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.mutablePreferencesOf
 import dev.marlonlom.apps.cappajv.core.preferences.UserPreferencesRepository
-import dev.marlonlom.apps.cappajv.core.preferences.UserSettings
 import dev.marlonlom.apps.cappajv.util.MainDispatcherRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito.mock
+import org.mockito.Mock
 import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 
 @ExperimentalCoroutinesApi
 internal class SettingsViewModelTest {
@@ -26,27 +35,56 @@ internal class SettingsViewModelTest {
 
   private lateinit var viewModel: SettingsViewModel
 
+  @Mock
+  private lateinit var mockDataStore: DataStore<Preferences>
+
+  @Before
+  fun setup() {
+    MockitoAnnotations.openMocks(this)
+  }
+
+
   @Test
-  fun `Should return valid settings from local storage`() = runTest {
-    val mockPreferencesRepository = mock(UserPreferencesRepository::class.java)
-    `when`(mockPreferencesRepository.userPreferencesFlow).thenReturn(
+  fun `Should return sampled settings from local storage`() = runTest {
+    `when`(mockDataStore.data).thenReturn(
       flowOf(
-        UserSettings(
-          useDarkTheme = false,
-          useDynamicColor = true,
-          isOnboarding = true
+        mutablePreferencesOf(
+          booleanPreferencesKey("dark_theme") to true,
+          booleanPreferencesKey("dynamic_colors") to true,
+          booleanPreferencesKey("is_onboarding") to false
         )
       )
     )
+    viewModel = SettingsViewModel(UserPreferencesRepository(mockDataStore))
+    val uiState = viewModel.uiState.first()
+    assertNotNull(uiState)
+    when (uiState) {
+      is SettingsUiState.Success -> {
+        assertNotNull(uiState.settings)
+        assertTrue(uiState.settings.useDarkTheme)
+        assertTrue(uiState.settings.useDynamicColor)
+        assertFalse(uiState.settings.isOnboarding)
+      }
 
-    viewModel = SettingsViewModel(mockPreferencesRepository)
+      else -> fail()
+    }
+  }
 
-    try {
-      val state = viewModel.uiState.first()
-      Assert.assertNotNull(state)
-    } catch (e: Exception) {
-      println(e.message)
-      Assert.fail()
+  @Test
+  fun `Should return default settings from local storage`() = runTest {
+    `when`(mockDataStore.data).thenReturn(flowOf(emptyPreferences()))
+    viewModel = SettingsViewModel(UserPreferencesRepository(mockDataStore))
+    val uiState = viewModel.uiState.first()
+    assertNotNull(uiState)
+    when (uiState) {
+      is SettingsUiState.Success -> {
+        assertNotNull(uiState.settings)
+        assertFalse(uiState.settings.useDarkTheme)
+        assertTrue(uiState.settings.useDynamicColor)
+        assertTrue(uiState.settings.isOnboarding)
+      }
+
+      else -> fail()
     }
   }
 }
