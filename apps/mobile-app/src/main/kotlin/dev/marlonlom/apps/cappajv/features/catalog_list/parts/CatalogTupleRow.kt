8dde0da80c3e986cd8dc64a5a5c5fc89aebb211a
Marlon López <malm.gm@gmail.com>
2024-03-24 11:50:03 -0500

    feat(mobile-app): Updated catalog list row composable

--- apps/mobile-app/src/main/kotlin/dev/marlonlom/apps/cappajv/features/catalog_list/parts/CatalogTupleRow.kt
+++ apps/mobile-app/src/main/kotlin/dev/marlonlom/apps/cappajv/features/catalog_list/parts/CatalogTupleRow.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -18,8 +17,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SuggestionChip
-import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -28,16 +25,16 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.core.text.trimmedLength
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import dev.marlonlom.apps.cappajv.R
 import dev.marlonlom.apps.cappajv.core.database.entities.CatalogItemTuple
 import dev.marlonlom.apps.cappajv.ui.layout.DevicePosture
 import dev.marlonlom.apps.cappajv.ui.main.CappajvAppState
@@ -60,9 +57,6 @@ internal fun CatalogTupleRow(
   onCatalogItemTupleClicked: (Long) -> Unit,
   modifier: Modifier = Modifier,
 ) {
-
-  val imageSizeDp = getCatalogTupleImageSizeDp(appState)
-
   Card(
     modifier = modifier.fillMaxWidth(),
     shape = CardDefaults.outlinedShape,
@@ -72,103 +66,127 @@ internal fun CatalogTupleRow(
     },
   ) {
     Row(
-      modifier.padding(10.dp),
+      modifier = modifier.padding(vertical = 10.dp),
       verticalAlignment = Alignment.CenterVertically,
-      horizontalArrangement = Arrangement.spacedBy(20.dp)
+      horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-      val imageRequest = ImageRequest.Builder(LocalContext.current)
-        .data(tuple.picture)
-        .crossfade(true)
-        .build()
-
-      AsyncImage(
-        model = imageRequest,
-        contentDescription = tuple.title,
-        contentScale = ContentScale.Crop,
-        modifier = Modifier
-          .border(
-            width = 2.dp,
-            color = MaterialTheme.colorScheme.secondary,
-            shape = MaterialTheme.shapes.medium,
-          )
-          .clip(MaterialTheme.shapes.medium)
-          .size(imageSizeDp)
-          .background(Color.White),
-      )
-
+      CatalogTuplePosterImage(tuple, appState)
       Column {
-        Text(
-          text = tuple.title,
-          style = getCatalogTupleTitleStyle(appState),
-          maxLines = 1,
-          overflow = TextOverflow.Ellipsis,
-          fontWeight = FontWeight.SemiBold
-        )
-        FlowRow(
-          horizontalArrangement = Arrangement.spacedBy(10.dp),
-        ) {
-          SuggestionChip(
-            onClick = { },
-            colors = SuggestionChipDefaults.suggestionChipColors(
-              containerColor = MaterialTheme.colorScheme.tertiaryContainer,
-              labelColor = MaterialTheme.colorScheme.onTertiaryContainer
-            ),
-            label = {
-              val samplePunctuationTxt = buildAnnotatedString {
-                val textParts = tuple.samplePunctuation.split(":")
-                withStyle(SpanStyle(fontWeight = FontWeight.SemiBold)) {
-                  append(textParts[0])
-                }
-                append(": ")
-                append(textParts[1].trim())
-              }
-              Text(text = samplePunctuationTxt)
-            },
-            shape = MaterialTheme.shapes.small,
-          )
-
-          if (tuple.punctuationsCount > 1) {
-            Text(
-              text = gePunctuationCountText(
-                appState = appState,
-                punctuationsCount = tuple.punctuationsCount - 1
-              ),
-              modifier = Modifier.align(Alignment.CenterVertically)
-            )
-          }
-        }
+        CatalogTupleTitle(tuple, appState)
+        CatalogTupleSamplePunctuationText(tuple)
       }
     }
   }
 }
 
 @Composable
-private fun gePunctuationCountText(
-  appState: CappajvAppState,
-  punctuationsCount: Int,
-) = when {
-  appState.isCompactWidth.and(appState.isLandscape.not())
-    .and(appState.devicePosture is DevicePosture.Separating.TableTop) -> "+$punctuationsCount"
+private fun CatalogTupleSamplePunctuationText(
+  tuple: CatalogItemTuple
+) {
+  val samplePunctuationTxt = buildAnnotatedString {
+    val textParts = tuple.samplePunctuation.split(":")
+    withStyle(
+      SpanStyle(
+        fontWeight = FontWeight.SemiBold,
+        color = MaterialTheme.colorScheme.secondary
+      )
+    ) {
+      val punctuationTitle = when {
+        textParts[0].trimmedLength() > 10 -> textParts[0].split(" ")[0].plus(" ...")
+        else -> textParts[0]
+      }
+      append(punctuationTitle)
+    }
+    append(": ")
+    append(textParts[1].trim())
+    if (tuple.punctuationsCount > 1) {
+      append(" ")
+      withStyle(
+        SpanStyle(
+          fontWeight = FontWeight.SemiBold,
+          color = MaterialTheme.colorScheme.secondary,
+        )
+      ) {
+        append("(+ ${tuple.punctuationsCount - 1})")
+      }
+    }
+  }
+  Text(
+    text = samplePunctuationTxt,
+    style = MaterialTheme.typography.labelMedium,
+  )
+}
 
-  else -> stringResource(
-    R.string.text_catalog_hint_more_punctuations,
-    punctuationsCount
+/**
+ * Catalog tuple item title composable text.
+ *
+ * @author marlonlom
+ *
+ * @param tuple Catalog item tuple data.
+ * @param appState Application ui state.
+ */
+@Composable
+private fun CatalogTupleTitle(
+  tuple: CatalogItemTuple,
+  appState: CappajvAppState
+) {
+  Text(
+    text = tuple.title,
+    style = getCatalogTupleTitleStyle(appState),
+    color = MaterialTheme.colorScheme.secondary,
+    maxLines = 1,
+    overflow = TextOverflow.Ellipsis,
+    fontWeight = FontWeight.SemiBold
+  )
+}
+
+/**
+ * Catalog tuple item poster image composable.
+ *
+ * @author marlonlom
+ *
+ * @param tuple Catalog item tuple data.
+ * @param appState Application ui state.
+ */
+@Composable
+private fun CatalogTuplePosterImage(
+  tuple: CatalogItemTuple,
+  appState: CappajvAppState
+) {
+  val imageRequest = ImageRequest.Builder(LocalContext.current)
+    .data(tuple.picture)
+    .crossfade(true)
+    .build()
+
+  AsyncImage(
+    model = imageRequest,
+    contentDescription = tuple.title,
+    contentScale = ContentScale.Crop,
+    modifier = Modifier
+      .border(
+        width = 2.dp,
+        color = MaterialTheme.colorScheme.secondary,
+        shape = MaterialTheme.shapes.medium,
+      )
+      .clip(MaterialTheme.shapes.medium)
+      .size(getCatalogTupleImageSizeDp(appState))
+      .background(Color.White),
   )
 }
 
 @Composable
-private fun getCatalogTupleImageSizeDp(appState: CappajvAppState) = when {
+private fun getCatalogTupleImageSizeDp(appState: CappajvAppState): DpSize = when {
   appState.isCompactWidth.and(appState.isLandscape.not())
-    .and(appState.devicePosture is DevicePosture.Separating.TableTop) -> 56.dp
+    .and(appState.devicePosture is DevicePosture.Separating.TableTop) -> DpSize(48.dp, 56.dp)
 
-  else -> 100.dp
+  else -> DpSize(56.dp, 64.dp)
 }
 
 
 @Composable
 private fun getCatalogTupleTitleStyle(appState: CappajvAppState) = when {
   appState.isCompactWidth.and(appState.isLandscape.not())
-    .and(appState.devicePosture is DevicePosture.Separating.TableTop) -> MaterialTheme.typography.titleMedium
+    .and(appState.devicePosture is DevicePosture.Separating.TableTop) -> MaterialTheme.typography.bodyMedium
 
-  else -> MaterialTheme.typography.titleLarge
+  else -> MaterialTheme.typography.bodyLarge
 }
