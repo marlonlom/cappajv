f16c87bb6bfadcda403325461df29043360b7bbc
Marlon López <malm.gm@gmail.com>
2024-03-26 15:06:54 -0500

    feat(mobile-app): Added catalog favorites default ui screen

--- apps/mobile-app/src/main/kotlin/dev/marlonlom/apps/cappajv/features/catalog_favorites/screens/DefaultPortraitCatalogFavoritesScreen.kt
+++ apps/mobile-app/src/main/kotlin/dev/marlonlom/apps/cappajv/features/catalog_favorites/screens/DefaultPortraitCatalogFavoritesScreen.kt
@@ -1,2 +1,59 @@
+/*
+ * Copyright 2024 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package dev.marlonlom.apps.cappajv.features.catalog_favorites.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.marlonlom.apps.cappajv.features.catalog_favorites.CatalogFavoritesUiState
+import dev.marlonlom.apps.cappajv.features.catalog_favorites.parts.CatalogFavoritesHeadline
+import dev.marlonlom.apps.cappajv.features.catalog_favorites.slots.CatalogFavoritesResultsSlot
+import dev.marlonlom.apps.cappajv.ui.main.CappajvAppState
+
+/**
+ * Default portrait catalog favorites screen composable ui.
+ *
+ * @author Marlonlom
+ *
+ * @param appState Application ui state.
+ * @param favoritesListState Catalog favorites ui state.
+ * @param isRouting True/False if should navigate through routing.
+ * @param onFavoriteItemClicked Action for favorite item clicked.
+ * @param onFavoriteItemRemoved Action for favorite item removed.
+ */
+@ExperimentalFoundationApi
+@Composable
+fun DefaultPortraitCatalogFavoritesScreen(
+  appState: CappajvAppState,
+  favoritesListState: CatalogFavoritesUiState,
+  isRouting: Boolean,
+  onFavoriteItemClicked: (Long, Boolean) -> Unit,
+  onFavoriteItemRemoved: (Long) -> Unit,
+) {
+  val contentHorizontalPadding = when {
+    appState.isLandscape.not().and(appState.isMediumWidth) -> 40.dp
+    appState.isLandscape.not().and(appState.isExpandedWidth) -> 80.dp
+    else -> 20.dp
+  }
+  Column(
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(contentHorizontalPadding)
+  ) {
+    CatalogFavoritesHeadline(appState)
+    CatalogFavoritesResultsSlot(
+      appState = appState,
+      favoritesListState = favoritesListState,
+      isRouting = isRouting,
+      onFavoriteItemClicked = onFavoriteItemClicked,
+      onFavoriteItemRemoved = onFavoriteItemRemoved,
+    )
+  }
+}
