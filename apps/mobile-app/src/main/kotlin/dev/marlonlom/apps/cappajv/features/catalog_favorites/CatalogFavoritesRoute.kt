59083dc9ca02e8c4d9fbc2a1dbd1407d993785ca
Marlon López <malm.gm@gmail.com>
2024-03-27 16:39:26 -0500

    feat(mobile-app): Updated catalog favorites composable screens

--- apps/mobile-app/src/main/kotlin/dev/marlonlom/apps/cappajv/features/catalog_favorites/CatalogFavoritesRoute.kt
+++ apps/mobile-app/src/main/kotlin/dev/marlonlom/apps/cappajv/features/catalog_favorites/CatalogFavoritesRoute.kt
@@ -20,7 +20,7 @@ import org.koin.androidx.compose.koinViewModel
  * @author marlonlom
  *
  * @param appState Application ui state.
-* @param appContentCallbacks Application content callbacks.
+ * @param appContentCallbacks Application content callbacks.
  * @param viewModel Catalog favorites viewmodel.
  */
 @ExperimentalFoundationApi
