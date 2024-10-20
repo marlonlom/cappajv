0f074c9991944d60363a249d86fbeb701052e8a3
Marlon López <malm.gm@gmail.com>
2024-10-20 00:31:16 -0500

    refactor: change lato font with albersans font, delete old branded theme

--- apps/mobile/src/main/kotlin/dev/marlonlom/cappajv/ui/theme/CappajvShapes.kt
+++ apps/mobile/src/main/kotlin/dev/marlonlom/cappajv/ui/theme/CappajvShapes.kt
@@ -15,8 +15,10 @@ import androidx.compose.ui.unit.dp
  *
  * @author marlonlom
  */
-internal val CappaShapes = Shapes(
+internal val CappajvShapes = Shapes(
+  extraSmall = RoundedCornerShape(4.dp),
   small = RoundedCornerShape(8.dp),
-  medium = RoundedCornerShape(8.dp),
-  large = RoundedCornerShape(10.dp)
+  medium = RoundedCornerShape(12.dp),
+  large = RoundedCornerShape(16.dp),
+  extraLarge = RoundedCornerShape(28.dp)
 )
