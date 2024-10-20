1d50d67382503092d3827b8e6d69d5ebff8f04c7
Marlon López <malm.gm@gmail.com>
2024-10-20 00:32:15 -0500

    refactor: add new branded theme with color contrasts

--- apps/mobile/src/main/kotlin/dev/marlonlom/cappajv/ui/theme/CappajvFont.kt
+++ apps/mobile/src/main/kotlin/dev/marlonlom/cappajv/ui/theme/CappajvFont.kt
@@ -7,14 +7,10 @@ package dev.marlonlom.cappajv.ui.theme
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineBreak
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.unit.sp
 import dev.marlonlom.cappajv.R
 
 /**
@@ -23,115 +19,35 @@ import dev.marlonlom.cappajv.R
  * @author marlonlom
  */
 @ExperimentalMaterial3Api
-internal object BrandFont {
+internal object CappajvFont {
 
-  private val DEFAULT_FONT = FontFamily(
-    Font(resId = R.font.lato_regular, weight = FontWeight.Normal),
-    Font(resId = R.font.lato_italic, weight = FontWeight.Normal, style = FontStyle.Italic),
-    Font(resId = R.font.lato_bold, weight = FontWeight.Bold),
-    Font(resId = R.font.lato_bold_italic, style = FontStyle.Italic),
+  /** Default font family. */
+  private val defaultFontFamily = FontFamily(
+    Font(resId = R.font.albertsans_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.albertsans_italic, weight = FontWeight.Normal, style = FontStyle.Italic),
+    Font(resId = R.font.albertsans_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.albertsans_bold_italic, style = FontStyle.Italic),
   )
 
-  private val DEFAULT_TEXT_STYLE = TextStyle(
-    fontFamily = DEFAULT_FONT,
-    fontWeight = FontWeight.Normal,
-    lineHeightStyle = LineHeightStyle(
-      alignment = LineHeightStyle.Alignment.Center,
-      trim = LineHeightStyle.Trim.None
-    )
-  )
-
-  private const val NEGATIVE_ZERO_DOT_TWENTY_FIVE = -0.25
+  /** Baseline typography. */
+  private val baseline = Typography()
 
   /** Set of Material typography styles to start with. */
   val appTypography = Typography(
-    displayLarge = DEFAULT_TEXT_STYLE.copy(
-      fontSize = 57.sp,
-      lineHeight = 64.sp,
-      letterSpacing = NEGATIVE_ZERO_DOT_TWENTY_FIVE.sp
-    ),
-    displayMedium = DEFAULT_TEXT_STYLE.copy(
-      fontSize = 45.sp,
-      lineHeight = 52.sp,
-      letterSpacing = 0.sp
-    ),
-    displaySmall = DEFAULT_TEXT_STYLE.copy(
-      fontSize = 36.sp,
-      lineHeight = 44.sp,
-      letterSpacing = 0.sp
-    ),
-    headlineLarge = DEFAULT_TEXT_STYLE.copy(
-      fontSize = 32.sp,
-      lineHeight = 40.sp,
-      letterSpacing = 0.sp,
-      lineBreak = LineBreak.Heading,
-    ),
-    headlineMedium = DEFAULT_TEXT_STYLE.copy(
-      fontSize = 28.sp,
-      lineHeight = 36.sp,
-      letterSpacing = 0.sp,
-      lineBreak = LineBreak.Heading,
-    ),
-    headlineSmall = DEFAULT_TEXT_STYLE.copy(
-      fontSize = 24.sp,
-      lineHeight = 32.sp,
-      letterSpacing = 0.sp,
-      lineBreak = LineBreak.Heading,
-    ),
-    titleLarge = DEFAULT_TEXT_STYLE.copy(
-      fontSize = 22.sp,
-      lineHeight = 28.sp,
-      letterSpacing = 0.sp,
-      lineBreak = LineBreak.Heading,
-    ),
-    titleMedium = DEFAULT_TEXT_STYLE.copy(
-      fontSize = 16.sp,
-      lineHeight = 24.sp,
-      letterSpacing = 0.15.sp,
-      lineBreak = LineBreak.Heading,
-    ),
-    titleSmall = DEFAULT_TEXT_STYLE.copy(
-      fontWeight = FontWeight.Medium,
-      fontSize = 14.sp,
-      lineHeight = 20.sp,
-      letterSpacing = 0.1.sp,
-      lineBreak = LineBreak.Heading,
-    ),
-    bodyLarge = DEFAULT_TEXT_STYLE.copy(
-      fontSize = 16.sp,
-      lineHeight = 24.sp,
-      letterSpacing = 0.5.sp,
-      lineBreak = LineBreak.Heading,
-    ),
-    bodyMedium = DEFAULT_TEXT_STYLE.copy(
-      fontSize = 14.sp,
-      lineHeight = 20.sp,
-      letterSpacing = 0.25.sp,
-      lineBreak = LineBreak.Heading,
-    ),
-    bodySmall = DEFAULT_TEXT_STYLE.copy(
-      fontSize = 12.sp,
-      lineHeight = 16.sp,
-      letterSpacing = 0.4.sp,
-      lineBreak = LineBreak.Heading,
-    ),
-    labelLarge = DEFAULT_TEXT_STYLE.copy(
-      fontWeight = FontWeight.Medium,
-      fontSize = 14.sp,
-      lineHeight = 20.sp,
-      letterSpacing = 0.1.sp,
-    ),
-    labelMedium = DEFAULT_TEXT_STYLE.copy(
-      fontWeight = FontWeight.Medium,
-      fontSize = 12.sp,
-      lineHeight = 16.sp,
-      letterSpacing = 0.5.sp,
-    ),
-    labelSmall = DEFAULT_TEXT_STYLE.copy(
-      fontWeight = FontWeight.Medium,
-      fontSize = 11.sp,
-      lineHeight = 16.sp,
-      letterSpacing = 0.5.sp,
-    )
+    displayLarge = baseline.displayLarge.copy(fontFamily = defaultFontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = defaultFontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = defaultFontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = defaultFontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = defaultFontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = defaultFontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = defaultFontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = defaultFontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = defaultFontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = defaultFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = defaultFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = defaultFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = defaultFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = defaultFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = defaultFontFamily)
   )
 }
