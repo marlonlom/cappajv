321b24c102fb774009bc0f42d324929e69aca306
Marlon López <malm.gm@gmail.com>
2024-11-15 22:02:42 -0500

    fix: rename preferences core module

---
+++ features/core/preferences/src/main/kotlin/dev/marlonlom/cappajv/core/preferences/entities/UserColorContrasts.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2024 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package dev.marlonlom.cappajv.core.preferences.entities
+
+/**
+ * Color contrasts enum class.
+ *
+ * @author marlonlom
+ *
+ */
+enum class UserColorContrasts {
+  /** Enum instance for Standard color contrast. */
+  STANDARD,
+
+  /** Enum instance for Medium color contrast. */
+  MEDIUM,
+
+  /** Enum instance for High color contrast. */
+  HIGH,
+}
