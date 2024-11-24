135a5ccd670884d6dd60cbf745940ba3163bc170
Marlon López <malm.gm@gmail.com>
2024-11-23 21:50:21 -0500

    feat: add string utility functions

---
+++ apps/tv/src/main/kotlin/dev/marlonlom/cappajv/tv/ui/util/StringUtils.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2024 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package dev.marlonlom.cappajv.tv.ui.util
+
+import java.text.Normalizer
+import java.util.Locale
+
+inline val String.toSentenceCase: String
+  get() = this.lowercase(Locale.getDefault()).replaceFirstChar { it.uppercaseChar() }
+
+inline val String.slug: String
+  get() = Normalizer.normalize(this, Normalizer.Form.NFD)
+    .replace("\\p{Mn}+".toRegex(), "")
+    .lowercase(Locale.getDefault())
+    .replace("\n", " ")
+    .replace("[^a-z\\d\\s]".toRegex(), " ")
+    .split(" ")
+    .joinToString("-")
+    .replace("-+".toRegex(), "-")
