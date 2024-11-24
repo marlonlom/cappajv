d5cf68aaad1a8f719efedaffa1ea17424cd333fc
Marlon López <malm.gm@gmail.com>
2024-11-23 21:49:35 -0500

    feat: add koin module definitions into tv app

---
+++ apps/tv/src/main/kotlin/dev/marlonlom/cappajv/tv/di/AppModule.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2024 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package dev.marlonlom.cappajv.tv.di
+
+import dev.marlonlom.cappajv.core.catalog.CatalogDataService
+import dev.marlonlom.cappajv.core.database.CappaDatabase
+import dev.marlonlom.cappajv.core.database.datasource.LocalDataSource
+import dev.marlonlom.cappajv.core.database.datasource.LocalDataSourceImpl
+import dev.marlonlom.cappajv.tv.features.catalog.favorites.CatalogFavoritesRepository
+import dev.marlonlom.cappajv.tv.features.catalog.favorites.CatalogFavoritesViewModel
+import dev.marlonlom.cappajv.tv.features.catalog.home.CatalogHomeRepository
+import dev.marlonlom.cappajv.tv.features.catalog.home.CatalogHomeViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+import java.util.Locale
+
+/**
+ * Koin android tv application module definition object.
+ *
+ * @author marlonlom
+ *
+ */
+val appModule = module {
+  /* Data sources (local, remote) */
+  single<LocalDataSource> {
+    CappaDatabase.getInstance(androidContext()).let { db ->
+      LocalDataSourceImpl(
+        catalogItemsDao = db.catalogProductsDao(),
+        catalogPunctuationsDao = db.catalogPunctuationsDao(),
+        catalogFavoriteItemsDao = db.catalogFavoriteItemsDao(),
+        catalogSearchDao = db.catalogSearchDao(),
+      )
+    }
+  }
+  single<CatalogDataService> {
+    CatalogDataService(Locale.getDefault().language)
+  }
+  /* Repositories */
+  single<CatalogHomeRepository> {
+    CatalogHomeRepository(
+      localDataSource = get(),
+      catalogDataService = get(),
+    )
+  }
+  single<CatalogFavoritesRepository> {
+    CatalogFavoritesRepository(
+      localDataSource = get()
+    )
+  }
+  /* View models */
+  viewModelOf(::CatalogHomeViewModel)
+  viewModelOf(::CatalogFavoritesViewModel)
+}
