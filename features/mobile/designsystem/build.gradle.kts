c68787c6eb79e20506954bd934441cb2a2ef3bf3
Marlon López <malm.gm@gmail.com>
2025-04-13 14:49:07 -0500

    feat(mobile:designsystem): extract mobile app theme and resources

---
+++ features/mobile/designsystem/build.gradle.kts
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2024 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+plugins {
+  id("cappajv.android.library")
+  id("cappajv.android.lib.compose")
+  id("cappajv.spotless")
+}
+
+android {
+  namespace = "dev.marlonlom.cappajv.mobile.designsystem"
+}
