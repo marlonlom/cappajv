b40e2f9243ef1ac2c2a31e22dbcb38bec0dcee76
Marlon López <malm.gm@gmail.com>
2025-04-13 14:49:07 -0500

    feat(mobile:designsystem): extract mobile app theme and resources

---
+++ features/mobile/designsystem/src/main/kotlin/dev/marlonlom/cappajv/mobile/designsystem/theme/CappajvFont.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package dev.marlonlom.cappajv.mobile.designsystem.theme
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import dev.marlonlom.cappajv.mobile.designsystem.R
+
+/**
+ * Brand font object specification for application.
+ *
+ * @author marlonlom
+ */
+@ExperimentalMaterial3Api
+object CappajvFont {
+
+  /** Default font family. */
+  private val defaultFontFamily = FontFamily(
+    Font(resId = R.font.albertsans_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.albertsans_italic, weight = FontWeight.Normal, style = FontStyle.Italic),
+    Font(resId = R.font.albertsans_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.albertsans_bold_italic, style = FontStyle.Italic),
+  )
+
+  /** Baseline typography. */
+  private val baseline = Typography()
+
+  /** Set of Material typography styles to start with. */
+  val appTypography = Typography(
+    displayLarge = baseline.displayLarge.copy(fontFamily = defaultFontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = defaultFontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = defaultFontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = defaultFontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = defaultFontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = defaultFontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = defaultFontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = defaultFontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = defaultFontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = defaultFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = defaultFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = defaultFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = defaultFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = defaultFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = defaultFontFamily),
+  )
+}
---
+++ features/mobile/designsystem/src/main/kotlin/dev/marlonlom/cappajv/mobile/designsystem/theme/CappajvShapes.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2024 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package dev.marlonlom.cappajv.mobile.designsystem.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ * Shapes for Cappajv Application.
+ *
+ * @author marlonlom
+ */
+internal object CappajvShapes {
+
+  /** App Shapes configuration. */
+  val shapes = Shapes(
+    extraSmall = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(12.dp),
+    large = RoundedCornerShape(16.dp),
+    extraLarge = RoundedCornerShape(28.dp),
+  )
+}
