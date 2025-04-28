277e10bbbbfdfaa7f31d4e705a12a0f524243194
Marlon López <malm.gm@gmail.com>
2025-04-28 02:13:06 -0500

    refactor(apps:mobile): remove old dependencies, add android coil convention plugin

--- apps/mobile/build.gradle.kts
+++ apps/mobile/build.gradle.kts
@@ -6,57 +6,12 @@
 @file:Suppress("DSL_SCOPE_VIOLATION")
 
 plugins {
-  alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.android)
+  id("cappajv.android.application.mobile")
+  id("cappajv.android.app.compose.mobile")
+  id("cappajv.android.coil")
+  id("cappajv.android.koin")
+  id("cappajv.spotless")
   id("com.google.android.gms.oss-licenses-plugin")
-  alias(libs.plugins.compose.compiler)
-}
-
-android {
-  namespace = "dev.marlonlom.cappajv"
-  compileSdk = 35
-
-  defaultConfig {
-    applicationId = "dev.marlonlom.cappajv"
-    minSdk = 24
-    //noinspection OldTargetApi
-    targetSdk = 35
-    versionCode = 1
-    versionName = "1.0.0"
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    vectorDrawables {
-      useSupportLibrary = true
-    }
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro",
-      )
-    }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-
-  kotlinOptions {
-    jvmTarget = JavaVersion.VERSION_17.majorVersion
-  }
-
-  buildFeatures {
-    compose = true
-    buildConfig = true
-  }
-  packaging {
-    resources {
-      excludes += "/META-INF/{AL2.0,LGPL2.1}"
-    }
-  }
 }
 
 dependencies {
@@ -65,47 +20,25 @@ dependencies {
   implementation(project(":features:core:database"))
   implementation(project(":features:core:preferences"))
 
-  val composeBom = platform(libs.androidx.compose.bom)
-  implementation(composeBom)
-  androidTestImplementation(composeBom)
+  implementation(project(":features:mobile:catalog-detail"))
+  implementation(project(":features:mobile:catalog-favorites"))
+  implementation(project(":features:mobile:catalog-home"))
+  implementation(project(":features:mobile:designsystem"))
+  implementation(project(":features:mobile:onboarding"))
+  implementation(project(":features:mobile:settings"))
 
-  val koinBom = platform(libs.koin.bom)
-  implementation(koinBom)
-
-  implementation(libs.androidx.activity.compose)
   implementation(libs.androidx.browser)
-  implementation(libs.androidx.compose.ui)
-  implementation(libs.androidx.compose.ui.graphics)
-  implementation(libs.androidx.compose.ui.tooling.preview)
-  implementation(libs.androidx.compose.material.icons.extended)
-  implementation(libs.androidx.compose.material3)
-  implementation(libs.androidx.compose.material3.wsc)
-  implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.core.splashscreen)
-  implementation(libs.androidx.datastore.preferences)
+  implementation(libs.androidx.window.core)
+  implementation(libs.androidx.window)
+  implementation(libs.bundles.m3.adaptive) {
+    this.isTransitive = false
+  }
+
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.lifecycle.runtime.compose)
-  implementation(libs.androidx.navigation.compose)
-  implementation(libs.androidx.navigation.runtime.ktx)
-  implementation(libs.androidx.window)
-  implementation(libs.coil.compose)
   implementation(libs.google.oss.licenses)
   implementation(libs.jakewharton.timber)
   implementation(libs.koin.androidx.compose)
   implementation(libs.koin.androidx.compose.navigation)
-  implementation(libs.kotlinx.coroutines.android)
-  implementation(libs.kotlinx.coroutines.core)
-  implementation(libs.bundles.database.room)
-
-  testImplementation(libs.junit)
-  testImplementation(libs.kotlinx.coroutines.test)
-  testImplementation(libs.mockito.kotlin)
-
-  androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-  androidTestImplementation(libs.androidx.test.ext.junit)
-  androidTestImplementation(libs.androidx.test.espresso.core)
-  androidTestImplementation(libs.google.truth)
-
-  debugImplementation(libs.androidx.compose.ui.tooling)
-  debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
