3960cf0cf9af8c9703ebfb0b12549d593d6480b8
Marlon López <malm.gm@gmail.com>
2025-05-04 23:59:55 -0500

    feat(domain:catalog-detail): implement catalog detail domain module

--- features/domain/catalog-detail/src/main/kotlin/dev/marlonlom/cappajv/domain/catalog/detail/CatalogDetailRepository.kt
+++ features/domain/catalog-detail/src/main/kotlin/dev/marlonlom/cappajv/domain/catalog/detail/CatalogDetailRepository.kt
@@ -2,7 +2,7 @@
  * Copyright 2024 Marlonlom
  * SPDX-License-Identifier: Apache-2.0
  */
-package dev.marlonlom.cappajv.mobile.catalog.detail.domain
+package dev.marlonlom.cappajv.domain.catalog.detail
 
 import dev.marlonlom.cappajv.core.database.datasource.LocalDataSource
 import dev.marlonlom.cappajv.core.database.entities.CatalogFavoriteItem
