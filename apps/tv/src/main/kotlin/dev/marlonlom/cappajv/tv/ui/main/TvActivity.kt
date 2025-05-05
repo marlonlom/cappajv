6bebfa631f48600fcfb4b606c97e72f194e3b8ed
Marlon López <malm.gm@gmail.com>
2025-05-05 02:34:00 -0500

    refactor(apps:tv): update theme implementation from designsystem tv module

--- apps/tv/src/main/kotlin/dev/marlonlom/cappajv/tv/ui/main/TvActivity.kt
+++ apps/tv/src/main/kotlin/dev/marlonlom/cappajv/tv/ui/main/TvActivity.kt
@@ -7,9 +7,9 @@ package dev.marlonlom.cappajv.tv.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dev.marlonlom.cappajv.tv.designsystem.theme.CappajvTvTheme
 import dev.marlonlom.cappajv.tv.ui.navigation.TvNavigationHost
 import dev.marlonlom.cappajv.tv.ui.rememberCappajvAppState
-import dev.marlonlom.cappajv.tv.ui.theme.CappajvTvTheme
 import org.koin.androidx.compose.KoinAndroidContext
 
 /**
