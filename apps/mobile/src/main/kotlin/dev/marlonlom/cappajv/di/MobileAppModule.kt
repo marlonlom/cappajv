d1cd1f2cea9b3d365779da58c1887838f1a766e6
Marlon López <malm.gm@gmail.com>
2025-05-04 20:53:05 -0500

    refactor(apps:mobile): update mobile app koin module definitions

---
+++ apps/mobile/src/main/kotlin/dev/marlonlom/cappajv/di/MobileAppModule.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2024 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package dev.marlonlom.cappajv.di
+
+import dev.marlonlom.cappajv.core.catalog.CatalogDataService
+import dev.marlonlom.cappajv.core.database.di.databaseKoinModule
+import dev.marlonlom.cappajv.core.preferences.di.preferencesKoinModule
+import dev.marlonlom.cappajv.mobile.catalog.detail.di.catalogDetailUiModule
+import dev.marlonlom.cappajv.mobile.catalog.favorites.di.catalogFavoritesUiModule
+import dev.marlonlom.cappajv.mobile.catalog.home.di.catalogHomeUiModule
+import dev.marlonlom.cappajv.mobile.settings.di.settingsUiModule
+import dev.marlonlom.cappajv.ui.main.MainActivityViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+import java.util.Locale
+
+/**
+ * Koin module providing dependencies related to the mobile app module.
+ *
+ * @author marlonlom
+ */
+val mobileAppModule = module {
+  single<CatalogDataService> { CatalogDataService(Locale.getDefault().language) } bind CatalogDataService::class
+
+  includes(databaseKoinModule, preferencesKoinModule)
+  includes(catalogHomeUiModule, catalogFavoritesUiModule, catalogDetailUiModule, settingsUiModule)
+
+  viewModelOf(::MainActivityViewModel)
+}
