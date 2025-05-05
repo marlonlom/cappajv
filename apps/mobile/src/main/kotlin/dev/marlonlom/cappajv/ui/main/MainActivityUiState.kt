d2e018fcd3c052723aed0ba0b523e790301ea69f
Marlon López <malm.gm@gmail.com>
2025-05-04 21:42:25 -0500

    refactor(apps:mobile): move main activity ui state helper functions for getting dark theme, dynamic colors and color contrast settings

--- apps/mobile/src/main/kotlin/dev/marlonlom/cappajv/ui/main/MainActivityUiState.kt
+++ apps/mobile/src/main/kotlin/dev/marlonlom/cappajv/ui/main/MainActivityUiState.kt
@@ -4,7 +4,10 @@
  */
 package dev.marlonlom.cappajv.ui.main
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
 import dev.marlonlom.cappajv.core.preferences.entities.UserSettings
+import dev.marlonlom.cappajv.mobile.designsystem.theme.CappajvColorContrasts
 
 /**
  * Represents the UI state for the MainActivity.
@@ -32,3 +35,49 @@ sealed interface MainActivityUiState {
    */
   data class Success(val userData: UserSettings) : MainActivityUiState
 }
+
+/**
+ * Determines whether dynamic color theming should be applied based on the [MainActivityUiState].
+ *
+ * Dynamic color is disabled during loading. If the state is successful, it reflects
+ * the user's preference for dynamic theming.
+ *
+ * @return `true` if dynamic color should be used; `false` otherwise.
+ */
+@Composable
+fun MainActivityUiState.shouldUseDynamicColor(): Boolean = when (this) {
+  MainActivityUiState.Loading -> false
+  is MainActivityUiState.Success -> this.userData.useDynamicColor
+}
+
+/**
+ * Determines whether the dark theme should be applied based on the [MainActivityUiState].
+ *
+ * If the state is loading, it falls back to the system dark theme setting.
+ * If the state is successful, it uses the user's theme preference, defaulting to
+ * the system setting if the user has disabled the custom dark theme.
+ *
+ * @return `true` if dark theme should be used; `false` otherwise.
+ */
+@Composable
+fun MainActivityUiState.shouldUseDarkTheme(): Boolean = when (this) {
+  MainActivityUiState.Loading -> isSystemInDarkTheme()
+  is MainActivityUiState.Success -> {
+    val useDarkTheme = this.userData.useDarkTheme
+    if (useDarkTheme.not()) isSystemInDarkTheme() else useDarkTheme
+  }
+}
+
+/**
+ * Determines the color contrast setting to be used based on the current [MainActivityUiState].
+ *
+ * If the UI is in a loading state, a standard color contrast is applied. If the UI is in a
+ * success state, the user's personalized color contrast setting is used.
+ *
+ * @return The name of the color contrast setting to apply as a [String].
+ */
+@Composable
+fun MainActivityUiState.shouldUseColorContrast(): String = when (this) {
+  MainActivityUiState.Loading -> CappajvColorContrasts.STANDARD
+  is MainActivityUiState.Success -> this.userData.colorContrast
+}.name
