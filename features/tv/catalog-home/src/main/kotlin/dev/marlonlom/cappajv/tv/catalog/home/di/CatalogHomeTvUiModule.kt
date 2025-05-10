f4961b11bd765d44410834f9d40867c77febdbd1
Marlon López <malm.gm@gmail.com>
2025-05-09 22:48:03 -0500

    feat(tv:catalog-home): implement catalog home screen feature for tv app

---
+++ features/tv/catalog-home/src/main/kotlin/dev/marlonlom/cappajv/tv/catalog/home/di/CatalogHomeTvUiModule.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2024 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package dev.marlonlom.cappajv.tv.catalog.home.di
+
+import dev.marlonlom.cappajv.core.catalog.CatalogDataService
+import dev.marlonlom.cappajv.core.database.datasource.LocalDataSource
+import dev.marlonlom.cappajv.domain.catalog.home.CatalogHomeRepository
+import dev.marlonlom.cappajv.tv.catalog.home.domain.CatalogHomeViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+/**
+ * Koin module providing dependencies related to the Catalog Home screen.
+ *
+ * @author marlonlom
+ */
+val catalogHomeTvUiModule = module {
+  single<CatalogHomeRepository> {
+    CatalogHomeRepository(
+      localDataSource = get<LocalDataSource>(),
+      catalogDataService = get<CatalogDataService>(),
+    )
+  } bind CatalogHomeRepository::class
+
+  viewModel {
+    CatalogHomeViewModel(get())
+  } bind CatalogHomeViewModel::class
+}
