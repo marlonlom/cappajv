570e48698a98c842e644cf576bf21bd07afce08c
Marlon López <malm.gm@gmail.com>
2025-05-11 17:21:51 -0500

    refactor(tv:catalog-home): fix focus management

--- features/tv/catalog-home/src/main/kotlin/dev/marlonlom/cappajv/tv/catalog/home/CatalogHomeTvScreen.kt
+++ features/tv/catalog-home/src/main/kotlin/dev/marlonlom/cappajv/tv/catalog/home/CatalogHomeTvScreen.kt
@@ -4,6 +4,7 @@
  */
 package dev.marlonlom.cappajv.tv.catalog.home
 
+import androidx.compose.foundation.focusGroup
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
@@ -12,10 +13,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusProperties
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -35,20 +38,21 @@ import org.koin.androidx.compose.koinViewModel
  *
  * @param onItemClicked A callback invoked when a catalog item is clicked,
  * providing the item's ID as a [Long].
- * @param focusRequester The [FocusRequester] used to request focus for initial or dynamic navigation,
- * ensuring a smooth user experience on TV platforms.
  * @param viewModel The [CatalogHomeViewModel] instance used to provide UI state and handle events.
  */
 @Composable
-fun CatalogHomeTvScreen(
-  onItemClicked: (Long) -> Unit,
-  focusRequester: FocusRequester,
-  viewModel: CatalogHomeViewModel = koinViewModel(),
-) {
+fun CatalogHomeTvScreen(onItemClicked: (Long) -> Unit, viewModel: CatalogHomeViewModel = koinViewModel()) {
   val uiState = viewModel.uiState.collectAsStateWithLifecycle()
   val categoriesArray = LocalContext.current.resources.getStringArray(R.array.array_categories)
+  val childFocusRequester = remember { FocusRequester() }
 
-  LazyColumn(modifier = Modifier.focusProperties { onEnter = { focusRequester.requestFocus() } }) {
+  LazyColumn(
+    modifier = Modifier.focusProperties {
+      onEnter = {
+        down = childFocusRequester
+      }
+    },
+  ) {
     when (uiState.value) {
       CatalogHomeUiState.Empty -> {}
 
@@ -75,6 +79,9 @@ fun CatalogHomeTvScreen(
             item { CategoryTitleSlot(category, categoryIndex) }
             item {
               LazyRow(
+                modifier = Modifier
+                  .focusRequester(childFocusRequester)
+                  .focusGroup(),
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
               ) {
                 items(
