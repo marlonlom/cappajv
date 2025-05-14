e46c435fec7eca305c9a56a139f69ef4174bb3c2
Marlon López <malm.gm@gmail.com>
2025-05-13 20:43:39 -0500

    refactor(tv:catalog-favorites): remove catalog core submodule

--- features/tv/catalog-favorites/build.gradle.kts
+++ features/tv/catalog-favorites/build.gradle.kts
@@ -16,7 +16,6 @@ android {
 }
 
 dependencies {
-  implementation(project(":features:core:catalog"))
   implementation(project(":features:core:database"))
   implementation(project(":features:domain:catalog-favorites"))
   implementation(project(":features:tv:designsystem"))
