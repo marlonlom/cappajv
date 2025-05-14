4e3be0e9861d74c729bba052352a7fe955a8a9de
Marlon López <malm.gm@gmail.com>
2025-05-13 22:03:32 -0500

    feat(apps:tv): include details koin module definition

--- apps/tv/src/main/kotlin/dev/marlonlom/cappajv/tv/di/TvAppModule.kt
+++ apps/tv/src/main/kotlin/dev/marlonlom/cappajv/tv/di/TvAppModule.kt
@@ -7,16 +7,10 @@ package dev.marlonlom.cappajv.tv.di
 import dev.marlonlom.cappajv.core.catalog.CatalogDataService
 import dev.marlonlom.cappajv.core.database.di.databaseKoinModule
 import dev.marlonlom.cappajv.core.preferences.di.preferencesKoinModule
+import dev.marlonlom.cappajv.tv.catalog.detail.di.catalogDetailTvUiModule
 import dev.marlonlom.cappajv.tv.catalog.favorites.di.catalogFavoritesTvUiModule
 import dev.marlonlom.cappajv.tv.catalog.home.di.catalogHomeTvUiModule
-import dev.marlonlom.cappajv.tv.features.catalog.details.CatalogDetailRepository
-import dev.marlonlom.cappajv.tv.features.catalog.details.CatalogDetailViewModel
-import dev.marlonlom.cappajv.tv.features.catalog.favorites.CatalogFavoritesRepository
-import dev.marlonlom.cappajv.tv.features.catalog.favorites.CatalogFavoritesViewModel
-import dev.marlonlom.cappajv.tv.features.catalog.home.CatalogHomeRepository
-import dev.marlonlom.cappajv.tv.features.catalog.home.CatalogHomeViewModel
 import dev.marlonlom.cappajv.tv.ui.main.TvActivityViewModel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -28,33 +22,11 @@ import java.util.Locale
  * @author marlonlom
  *
  */
-@OptIn(ExperimentalCoroutinesApi::class)
 val tvAppModule = module {
   single<CatalogDataService> { CatalogDataService(Locale.getDefault().language) } bind CatalogDataService::class
 
   includes(databaseKoinModule, preferencesKoinModule)
-  includes(catalogHomeTvUiModule, catalogFavoritesTvUiModule)
+  includes(catalogHomeTvUiModule, catalogFavoritesTvUiModule, catalogDetailTvUiModule)
 
-  /* Repositories */
-  single<CatalogHomeRepository> {
-    CatalogHomeRepository(
-      localDataSource = get(),
-      catalogDataService = get(),
-    )
-  }
-  single<CatalogFavoritesRepository> {
-    CatalogFavoritesRepository(
-      localDataSource = get(),
-    )
-  }
-  single<CatalogDetailRepository> {
-    CatalogDetailRepository(
-      localDataSource = get(),
-    )
-  }
-  /* View models */
-  viewModelOf(::CatalogHomeViewModel)
-  viewModelOf(::CatalogFavoritesViewModel)
-  viewModelOf(::CatalogDetailViewModel)
   viewModelOf(::TvActivityViewModel)
 }
