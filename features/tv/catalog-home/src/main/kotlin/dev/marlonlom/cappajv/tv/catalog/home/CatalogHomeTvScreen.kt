8bdc0d7c6de5671748584d51d9aa51c93c3f8407
Marlon López <malm.gm@gmail.com>
2025-05-15 21:16:46 -0500

    refactor(tv:catalog-home): use common loading content slot

--- features/tv/catalog-home/src/main/kotlin/dev/marlonlom/cappajv/tv/catalog/home/CatalogHomeTvScreen.kt
+++ features/tv/catalog-home/src/main/kotlin/dev/marlonlom/cappajv/tv/catalog/home/CatalogHomeTvScreen.kt
@@ -6,15 +6,12 @@ package dev.marlonlom.cappajv.tv.catalog.home
 
 import androidx.compose.foundation.focusGroup
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusProperties
@@ -22,13 +19,12 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.tv.material3.MaterialTheme
-import androidx.tv.material3.Text
 import dev.marlonlom.cappajv.core.database.entities.CatalogItemTuple
 import dev.marlonlom.cappajv.domain.catalog.home.CatalogHomeUiState
 import dev.marlonlom.cappajv.tv.catalog.home.domain.CatalogHomeViewModel
 import dev.marlonlom.cappajv.tv.designsystem.component.CatalogItemTvCard
 import dev.marlonlom.cappajv.tv.designsystem.slot.CategoryTitleSlot
+import dev.marlonlom.cappajv.tv.designsystem.slot.LoadingSlot
 import org.koin.androidx.compose.koinViewModel
 
 /**
@@ -57,19 +53,7 @@ fun CatalogHomeTvScreen(onItemClicked: (Long) -> Unit, viewModel: CatalogHomeVie
       CatalogHomeUiState.Empty -> {}
 
       CatalogHomeUiState.Loading -> {
-        item(
-          content = {
-            Box(
-              modifier = Modifier.fillMaxSize(),
-              contentAlignment = Alignment.Center,
-            ) {
-              Text(
-                text = "Loading...",
-                color = MaterialTheme.colorScheme.onBackground,
-              )
-            }
-          },
-        )
+        item { LoadingSlot() }
       }
 
       is CatalogHomeUiState.Success -> {
