7276b65a01249bc607cbf405bcabe930ce8c3fe9
Marlon López <malm.gm@gmail.com>
2025-05-16 03:13:18 -0500

    spotless(tv:catalog-detail): fix format violations

--- features/tv/catalog-detail/src/main/kotlin/dev/marlonlom/cappajv/tv/catalog/detail/slot/CatalogDetailTvContentSlot.kt
+++ features/tv/catalog-detail/src/main/kotlin/dev/marlonlom/cappajv/tv/catalog/detail/slot/CatalogDetailTvContentSlot.kt
@@ -1,8 +1,7 @@
 /*
- * Copyright 2025 Marlonlom
+ * Copyright 2024 Marlonlom
  * SPDX-License-Identifier: Apache-2.0
  */
-
 package dev.marlonlom.cappajv.tv.catalog.detail.slot
 
 import androidx.compose.foundation.background
@@ -16,14 +15,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Text
+import coil.ImageLoader
+import coil.imageLoader
 import dev.marlonlom.cappajv.core.database.entities.CatalogItem
 import dev.marlonlom.cappajv.core.database.entities.CatalogPunctuation
 import dev.marlonlom.cappajv.tv.catalog.detail.component.CatalogDetailTvLikeButton
 import dev.marlonlom.cappajv.tv.designsystem.component.CatalogItemTvImage
 
+/**
+ * Internal composable function to display the TV-specific content slot for a catalog item's detail screen.
+ *
+ * @param item The [CatalogItem] to display details for.
+ * @param points A list of [CatalogPunctuation] associated with the item.
+ * @param isFavorite Indicates if the item is currently marked as a favorite.
+ * @param onFavoriteChanged Callback triggered when the favorite state of the item changes.
+ * @param modifier Optional [Modifier] for styling and layout adjustments.
+ * @param imageLoader Custom [ImageLoader] for loading the image.
+ */
 @Composable
 internal fun CatalogDetailTvContentSlot(
   item: CatalogItem,
@@ -31,20 +43,20 @@ internal fun CatalogDetailTvContentSlot(
   isFavorite: Boolean,
   onFavoriteChanged: (CatalogItem, Boolean) -> Unit,
   modifier: Modifier = Modifier,
+  imageLoader: ImageLoader = LocalContext.current.imageLoader,
 ) {
   Column(modifier = modifier) {
-
     Row(horizontalArrangement = Arrangement.spacedBy(20.dp)) {
-
       CatalogItemTvImage(
         itemPicture = item.picture,
         itemTitle = item.title,
-        imageWidth = 100.dp
+        imageWidth = 100.dp,
+        imageLoader = imageLoader,
       )
 
       Column(
         horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
       ) {
         Text(
           modifier = Modifier
@@ -52,34 +64,33 @@ internal fun CatalogDetailTvContentSlot(
             .padding(vertical = 4.dp),
           text = item.category,
           style = MaterialTheme.typography.labelSmall,
-          color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f)
+          color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f),
         )
         Text(
           modifier = Modifier.padding(top = 4.dp),
           text = item.title,
           style = MaterialTheme.typography.headlineMedium,
-          color = MaterialTheme.colorScheme.onBackground
+          color = MaterialTheme.colorScheme.onBackground,
         )
         Text(
           modifier = Modifier.padding(top = 10.dp),
           text = item.detail,
           style = MaterialTheme.typography.bodySmall,
-          color = MaterialTheme.colorScheme.onBackground
+          color = MaterialTheme.colorScheme.onBackground,
         )
 
         CatalogDetailTvLikeButton(
           item = item,
           isFavorite = isFavorite,
-          onFavoriteChanged = onFavoriteChanged
+          onFavoriteChanged = onFavoriteChanged,
         )
       }
     }
 
     CatalogDetailTvPointsSlot(
-      points = points
+      points = points,
     )
 
     Spacer(Modifier.height(48.dp))
   }
-
 }
